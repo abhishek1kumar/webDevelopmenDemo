f613fee238bd0f7f1c8e72afedbd91209b168a1e
abhishek1kumar <abhishekgate33@gmail.com>
2023-08-05 01:15:31 +0530

    Add files via upload
    object added in local storage

--- localstrage.js
+++ localstrage.js
@@ -1,10 +1,20 @@
-function test(){
+function test(event){
+event.preventDefault();
 var uid =document.getElementById('name').value;
 var eid =document.getElementById('mail').value;
 var phoneid =document.getElementById('phone').value;
 console.log(phoneid);
 console.log('hello');
-localStorage.setItem('usename',uid);
-localStorage.setItem('email',eid);
-localStorage.setItem('phone', phoneid);
+const userInfo = {
+    username: uid,
+    email: eid,
+    phoneNo :phoneid
+    
+  }
+  localStorage.setItem('userInfo', JSON.stringify(userInfo));
+  console.log(localStorage.getItem(userInfo));
+  
+// localStorage.setItem('usename',uid);
+// localStorage.setItem('email',eid);
+// localStorage.setItem('phone', phoneid);
 }
